b6b3f01cb5b61327e5114ab0027fac3dc4ca0c5b
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2021-10-15 14:29:12 -0400

    Add project files.

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    class Calculator
+    {
+        /*
+         * A Basic calculator needs to be able to provide functionality for 
+         * Addition
+         * Multiplication
+         * Division
+         * Subtraction
+         * 
+         * The calculators class should be responsible for resolving mathematical formulas
+        */
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator programCalculator = new Calculator();
+        }
+    }
+}
